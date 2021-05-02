83663db5620e3ef4d226e0888489747f849dd2c3
itguny04 <the2133@naver.com>
2021-05-02 22:26:01 +0900

    DAY04

--- day04/create.php
+++ day04/create.php
@@ -28,7 +28,7 @@
         
         <!--제목 출력-->
         <h2>
-            <a href="web_app.php">WEB</a>
+            <a href="./web_app.php">WEB</a>
         </h2>
 
         <ol>
@@ -44,7 +44,7 @@
         <a href="create.php">create</a>
         <form action="create_process.php" method="post">
                 <p><input type="text" name="title" placeholder="title"></p>
-                <p><textarea name="description" placeholder="Description"></textarea></p>
+                <p><textarea name="description" placeholder="Description" cols="100" rows="25"></textarea></p>
                 <p><input type="submit" value="게시물 올리기"></p>
         </form>
 
